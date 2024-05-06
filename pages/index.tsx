4c0ea6851349c4ac14115f06e9655b684932b77c
Highzahawk <azhan2000@outlook.com>
2024-05-05 18:35:23 -0700

    Updated for colorblindness

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,7 +40,7 @@ export default function Home() {
         </div>
         <button
           onClick={reorderInterests}
-          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-300 my-5"
+          className="px-4 py-2 bg-blue-500 text-black rounded hover:bg-blue-700 transition duration-300 my-5" //change this?
           tabIndex={0}
         >
           Reorder Interests
